7e2ce9924a1a15225c50d95debce79b3dc5c741f
Алексей Бельченков <u608110@gmail.com>
2026-03-16 10:59:46 +0400

    add Toaster component to layout and wrap children with QueryProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 
 import "./globals.css";
+import { Toaster } from "sonner";
+import { QueryProvider } from "@/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,7 @@ export default function RootLayout({
           <html lang="en">
           <body className={inter.className}>
             <QueryProvider>
+              <Toaster />
               {children}
             </QueryProvider>
           </body>
