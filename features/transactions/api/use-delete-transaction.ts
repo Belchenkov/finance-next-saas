0e0e920e5e6ff2da4379981fd25d04762708b6a2
Алексей Бельченков <u608110@gmail.com>
2026-07-08 14:32:00 +0400

    avoid formatting-only diffs in use-delete-transaction.ts

--- features/transactions/api/use-delete-transaction.ts
+++ features/transactions/api/use-delete-transaction.ts
@@ -1,4 +1,4 @@
- import { toast } from "sonner";
+import { toast } from "sonner";
 import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
@@ -14,7 +14,7 @@ export const useDeleteTransaction = (id?: string) => {
     Error
   >({
     mutationFn: async () => {
-      const response = await client.api.transactions[":id"]["$delete"]({
+      const response = await client.api.transactions[":id"]["$delete"]({ 
         param: { id },
       });
       return await response.json();
